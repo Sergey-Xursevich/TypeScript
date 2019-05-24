35149a0f9291ec91bea2171c862e796012fdc339
Sergey Hursevich <hursik94@gmail.com>
2019-05-24 14:11:52 +0300

    add: object

--- index.ts
+++ index.ts
@@ -55,7 +55,32 @@ console.log(mySum(15, 10));
 
 
 
+/* Объект */
+type User = {name: string, age: number, getJobs: () => string[], jobs: string[], logName?: () => void}; //специльный объект с такими-то полями и типами
 
+let user: {name: string, age: number, getJobs: () => string[], jobs: string[], logName?: () => void} = {
+  name: 'WFM',
+  age: 50,
+  jobs: ['a', 'b'],
+  getJobs(): string[] {
+    return this.jobs;
+  },
+  logName(): void {
+    console.log(this.name);    
+  }
+};
+
+let user2: User = {
+  name: 'Max',
+  age: 30,
+  jobs: ['2'],
+  getJobs(): string[] {
+    return this.jobs;
+  },
+  logName(): void {
+    console.log(this.name);    
+  }
+}
 
 
 
